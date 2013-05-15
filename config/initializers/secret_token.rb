34183d0989e632cdd20b394a63b9e75ddd7a3a9c
LinhNT <linhnt@linhnt-pc.(none)>
2013-05-15 10:14:01 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolarisManage::Application.config.secret_token = '08ee3cda79f4c545c031ab096c0a0032447d9be89ac22a698500d489a38cf488fd3ae712d42d2c0f2dd0aea3fa227406516f019c900e74de252048bae524ea7e'
