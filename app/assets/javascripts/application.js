16b234596438fd7a4a8932d0652a264cbf1eec2e
LinhNT <linhnt@linhnt-pc.(none)>
2013-05-16 18:36:48 +0700

    update search user function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+(function() {
+  window.User = {
+    completeProjectLine: function(data) {
+      var html, watchs;
+
+      html = "";
+      html += "<div class='info' id ='solr_result'><a href=/promotions/" + data[1] + ">" + data[0] + "</a>" + "</div>";
+      
+      return html;
+    },
+    completeProjects: function(el) {
+      var hash;
+
+      hash = {
+        minChars: 1,
+        delay: 50,
+        width: 350,
+        scroll: false,
+        formatItem: function(data, i, total) {
+          return User.completeProjectLine(data);
+        }
+        
+      };
+      //console.log(hash);
+      return $(el).autocomplete("/users/search", hash).result(function(e, data, formatted) {
+        //location.href = "/promotions/" + data[1];
+        return false;
+      });
+    }
+  };
+
+  $(document).ready(function() {
+    return User.completeProjects(".searchbox input.query");
+  });
+
+}).call(this);
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,29 @@ class UsersController < ApplicationController
     @users = User.all
     @rows = Array.new
     @users.each do |user|
-      @rows << {"id" => user.id, "cell" => {"roman_name" => user.roman_name, "username" => user.username, "role_id" => user.role_id}}
+      @rows << {"id" => user.id, "cell" => {"roman_name" => user.roman_name,
+        "username" => user.username, "role_id" => user.role_id}}
     end
     @data = {"page" => 1, "total" => 1, "rows" =>@rows}
+  end
+  
+  def search
+    if params[:q].blank?
+      render :text => ""
+      return
+    end
+    params[:q].gsub!(/'/,'')
+    @search = User.search do
+      fulltext params[:q]
+    end
+    lines = @search.results.collect do |item|
+      puts item
+      "#{escape_javascript(item['username'])}#!##{item['id']}#!##{item['username']}#!##{item['username']}#!##{escape_javascript(item['username'])}"
+    end
+    if @search.results.count > 0
+      render :text => lines.join("\n")
+    else
+      render text: "test#!#0#!#test#!#test#!#test"
+    end
   end
 end
