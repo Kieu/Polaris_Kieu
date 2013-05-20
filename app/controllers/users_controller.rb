af47485cb4195ac439333ec65f2b12b068350405
DuyBQ <vntopcoders@gmail.com>
2013-05-20 09:35:55 +0700

    Update Paging function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,55 +2,57 @@ class UsersController < ApplicationController
   before_filter :signed_in_user
   before_filter :must_super
   
-  def index
+   def index
   end
 
-  def show
+   def show
   end
 
   def get_users_list
-    @users = User.where(del_flg: 0)
 
     rp = (params[:rp]).to_i
-    if(!rp)
+     if(!rp)
       rp = 10
     end
 
     #current page
     page = (params[:page]).to_i
-    if (!page)
+     if (!page)
       page = 1
     end
      start = ((page-1) * rp).to_i
- 
-    #get all Users
 
+    #get total records
+    count = User.where(status: 0).count
+    #total pages
+    total_pages = (count%rp > 0) ? (count/rp) + 1 : count/rp
+    #get all Users
     @users = User.where(status: 0).order('roman_name').limit(rp).offset(start)
     @rows = Array.new
-    @users.each do |user|
-      @rows << {"id" => user.id, "cell" => {"link" => "<a href='users/#{user.id}/edit'>Edit</a>","roman_name" => user.roman_name, 
+     @users.each do |user|
+       @rows << {"id" => user.id, "cell" => {"link" => "<a href='users/#{user.id}/edit'>Edit</a>","roman_name" => user.roman_name, 
                 "username" => user.username, "company" => user.company,
                 "email" => user.email, "role_id" =>Role.find( user.role_id).role_name}}
     end
-    @data = {"page" => 1, "total" => 1, "rows" =>@rows}
+    @data = {"page" => page, "total" => total_pages, "rows" =>@rows}
       render json: @data.to_json
     end
   end
   
   def search
-    if params[:q].blank?
+     if params[:q].blank?
       render :text => ""
       return
     end
     params[:q].gsub!(/'/,'')
-    @search = User.search do
+     @search = User.search do
       fulltext params[:q]
     end
-    lines = @search.results.collect do |item|
+     lines = @search.results.collect do |item|
       puts item
       "#{escape_javascript(item['username'])}#!##{item['id']}#!##{item['email']}#!##{item.role.role_name}#!##{escape_javascript(item['username'])}"
     end
-    if @search.results.count > 0
+     if @search.results.count > 0
       render :text => lines.join("\n")
     else
       render text: "test#!#0#!#test#!#test#!#test"
