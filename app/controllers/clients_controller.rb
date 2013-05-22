8d6ba4145c27edd97a2092e34b670e985789a420
bangvk <khanhbang_ptit@yahoo.com>
2013-05-22 15:15:03 +0700

    fix bug for client list screen

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -14,6 +14,9 @@ class ClientsController < ApplicationController
 		    @clients << client_user.client
 		  end
 	  end
+
+    # get client name of 1st client
+    @clientName = @clients[0].client_name
   end
   
   #get list promotion and paging
@@ -30,43 +33,61 @@ class ClientsController < ApplicationController
 	  end
     
 	  # get params for paging
+    if(params[:id] != '')
+      clientId = params[:id]
+    else
+      clientId = @clients[0].id
+    end
+
 	  # row per page
 	  rp = (params[:rp]).to_i
-	  if(!rp)
+	  if(!rp || rp != 10 || rp != 30 || rp != 50 || rp != 100)
       rp = 10
     end
 	
 	  #current page
-	  page = (params[:page]).to_i
+	  page = params[:page].to_i
 	  if (!page)
       page = 1
     end
 	  start = ((page-1) * rp).to_i
 	
 	  @promotions = Array.new
+
+    count = Promotion.where("client_id = ? ", clientId).order('promotion_name').count
+
 	  #get promotion for each client
-	  @clients.each do |client|
-	    aryPromotion = Promotion.where("client_id = ? ", client.id).order('promotion_name').limit(rp).offset(start)
-	    aryPromotion.each do |promotion|
-	      @promotions << {'id' => promotion.id, 'cell' => {'promotion_name' => promotion.promotion_name}}
-	    end
-	  end
+	  aryPromotion = Promotion.where("client_id = ? ", clientId).order('promotion_name').limit(rp).offset(start)
+    aryPromotion.each do |promotion|
+      @promotions << {'id' => promotion.id, 'cell' => {'promotion_name' => promotion.promotion_name}}
+    end
 	
-	  @data = {"page" => 1, "total" => 1, "rows" => @promotions}
-    render json: @data.to_json
+	  @data = {page: page, total: count, rows: @promotions}
+    render json: @data
   end
   
   def show
-    #get list clients
+    # get client list
     if(current_user.role_id == Settings.role.SUPER)
-	    @clients = Client.find(:all, :order => 'roman_name', :conditions => ['del_flg = 0'])
-	  elsif(current_user.role_id == Settings.role.AGENCY)
-	    @clients = Array.new
-	    aryClientUser = current_user.client_users.where(' del_flg = 0 ')
-	    aryClientUser.each do |client_user|
-		    @clients << client_user.client
-		  end
-	  end
+      @clients = Client.find(:all, :order => 'roman_name', :conditions => ['del_flg = 0'])
+    elsif(current_user.role_id == Settings.role.AGENCY)
+      @clients = Array.new
+      aryClientUser = current_user.client_users.where(' del_flg = 0 ')
+      aryClientUser.each do |client_user|
+        @clients << client_user.client
+      end
+    end
+
+    # get current client name
+    @clients.each do |client|
+      if(client.id == params[:id].to_i)
+        @clientName = client.client_name
+      end
+    end
+
+    @clientId = params[:id]
+    render :index
+
   end
 
   def new
