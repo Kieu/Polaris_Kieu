4446423512aa60a4054a802a4820fea021ec903f
DuyBQ <vntopcoders@gmail.com>
2013-05-24 12:42:24 +0700

    Update Account register

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,15 +4,18 @@ class AccountsController < ApplicationController
   def new
     @account = Account.new
     @promotion_id= params[:promotion_id]
+    @promotion = Promotion.find_by_id(@promotion_id)
   end
   
   def create
     @account = Account.new(params[:account])
     @promotion_id= params[:account][:promotion_id]
+    #get promotion by id
+    @promotion = Promotion.find_by_id(@promotion_id)
     @account.create_user_id = current_user.id
     if @account.save
       flash[:success] = "Client was successfully created"
-      redirect_to new_account_path
+      redirect_to promotions_path
     else
       render :new
     end
