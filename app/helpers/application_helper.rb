5b3825b481c03fa2d87977664724d426f4362c9e
Khanh Le <le.duy.khanh@framgia.com>
2013-05-29 17:44:59 +0700

    fix bugs #2477 #2478 #2482

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def short_name name
+    if name.length > Settings.MAX_LENGTH_NAME
+      name = name.first(Settings.MAX_LENGTH_NAME) + "..."
+    end
+    name
+  end
 end
