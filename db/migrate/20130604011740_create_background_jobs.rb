9465df39e83e7553d8a922ab2c1f42d677776902
quyetdd <duyquyetdo.scom@gmail.com>
2013-06-04 08:27:27 +0700

    Update 20130604011740_create_background_jobs.rb

--- db/migrate/20130604011740_create_background_jobs.rb
+++ db/migrate/20130604011740_create_background_jobs.rb
@@ -3,7 +3,7 @@ class CreateBackgroundJobs < ActiveRecord::Migration
     create_table :background_jobs do |t|
       t.integer :user_id, limit: 11
       t.string :filename, limit: 512
-      t.type_view :type_view, limit: 255
+      t.string :type_view, limit: 255
       t.string :status, limit: 1
 
       t.timestamps
