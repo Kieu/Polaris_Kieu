fb508c9c3eede0b77617b83796a7f3bf8740b639
DuyBQ <vntopcoders@gmail.com>
2013-06-06 14:05:07 +0700

    develop

---
+++ app/assets/javascripts/resizetable.js
@@ -0,0 +1,81 @@
+function tableResize(){
+	//可変対象となるtable
+	var className = ".hDivBox";
+
+	//長さをとってくるtable
+	var getclassName = ".bDiv";
+
+	//fixさせるテーブルの数
+	var num = parseInt($(className).length);
+	//セレクタを生成して配列にセット
+
+	//eq関数用の変数
+	var j;
+
+	//セルの長さ
+	var cell;
+
+	//セルのwidth
+	var cellWidth;
+
+	//tableのwidth
+	var tableWidth;
+
+	//ループ用
+	var i;
+	var k;
+
+	//最小サイズ
+	var min = 20;
+	console.log(num);
+	//テーブルごとにリサイズ処理する
+	for(i = 0;i < num;i++){
+		cell = "";
+		tableWidth = "";
+		cellWidth = "";
+
+		cell = parseInt($(className).eq(i).find("tr th").length);
+
+		var userAgent = window.navigator.userAgent.toLowerCase();
+				
+		for(k = 1;k < cell;k++){
+			if($(getclassName).find("tr td").eq(k).css("display") == "none"){
+				continue;
+			}
+
+			cellWidth = 0;
+
+			//chromeはなぜか1px不明なwidthをとられる
+			if(userAgent.indexOf('chrome') != -1){
+				cellWidth = 
+					$(getclassName).eq(i).find("tr td").eq(k).outerWidth() -
+					parseInt($(getclassName).eq(i).find("tr td div").eq(k).css("border-left-width")) -
+					parseInt($(getclassName).eq(i).find("tr td div").eq(k).css("border-right-width")) + 1;
+			//safariはouterWidthでborderが取れないのかも
+			}else if (userAgent.indexOf('safari') != -1) {
+				cellWidth = 
+					$(getclassName).eq(i).find("tr td div").eq(k).outerWidth();
+			//その他
+			}else{
+				
+				cellWidth = 
+					parseInt($(getclassName).eq(i).find("tr td div").eq(k).width()) -
+					parseInt($(getclassName).eq(i).find("tr td div").eq(k).css("border-left-width")) -
+					parseInt($(getclassName).eq(i).find("tr td div").eq(k).css("border-right-width"));
+					console.log($(getclassName).eq(i).find("tr td div").eq(k));
+					console.log($(getclassName).eq(i).find("tr td div").eq(k).width());
+			}
+			
+			if(cellWidth < min){
+				$(getclassName).eq(i).find("tr td").eq(k).css("width",min)
+				cellWidth = min;
+			}
+			$(className).eq(i).find("tr th").eq(k).css("width",(cellWidth));
+			$(getclassName).eq(i).find("tr td").eq(k).css("width",(cellWidth));
+		}
+
+		tableWidth = parseInt($(getclassName).eq(i).innerWidth());
+		$(className).eq(i).find("table").css("width",tableWidth -18);
+
+	}
+}
