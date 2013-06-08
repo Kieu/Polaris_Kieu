d4560ac1c94c459ba0f48329598799f2b3b11147
Hoang Quan <hoang.van.quan@framgia.com>
2013-06-08 13:19:00 +0700

    Account edit

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,9 +7,26 @@ class AccountsController < ApplicationController
     @promotion = Promotion.find_by_id(@promotion_id)
   end
   
+  def edit
+    @account = Account.find(params[:id])
+    @promotion_id = params[:promotion_id]
+    @promotion = Promotion.find_by_id(@promotion_id)
+  end
+  def update
+    @account = Account.find(params[:id])
+    @promotion_id = params[:promotion_id]
+    @promotion = Promotion.find_by_id(@promotion_id)
+    @account.create_user_id = current_user.id
+    if @account.update_attributes(params[:account])
+      flash[:success] = I18n.t("account.flash_messages.success")
+      redirect_to promotions_path
+    else
+      render :edit
+    end
+  end
   def create
     @account = Account.new(params[:account])
-    @promotion_id = params[:account][:promotion_id]
+    @promotion_id = params[:promotion_id]
     #get promotion by id
     @promotion = Promotion.find_by_id(@promotion_id)
     @account.create_user_id = current_user.id
@@ -17,7 +34,7 @@ class AccountsController < ApplicationController
       flash[:success] = "Client was successfully created"
       redirect_to promotions_path
     else
-      render :new
+      render "new", promotion_id: @promotion_id
     end
   end
 end
