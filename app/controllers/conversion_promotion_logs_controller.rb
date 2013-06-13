eec5ec15dd39c78cb00db812679dd3610edcab5b
quyetdd <quyetdd@aveglobal.net>
2013-06-13 16:03:09 +0700

    export conversion log

--- app/controllers/conversion_promotion_logs_controller.rb
+++ app/controllers/conversion_promotion_logs_controller.rb
@@ -31,9 +31,9 @@ def download_csv
     background_job = BackgroundJob.create
     job_id = ExportConversionLogsData.create(user_id: current_user.id,
       promotion_id: params[:promotion_id].to_i,
-      conversion_id: params[:conversion_id].to_i,
-      media_category_id: params[:media_category_id].to_i,
-      account_id: params[:account_id].to_i, start_date: cookies[:s],
+      conversion_id: params[:conversion_id],
+      media_category_id: params[:media_category_id],
+      account_id: params[:account_id], start_date: cookies[:s],
       end_date: cookies[:e], show_error: cookies[:ser],
       bgj_id: background_job.id)
     background_job.job_id = job_id
