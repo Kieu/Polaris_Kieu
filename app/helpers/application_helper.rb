3ec99dae74144514cccbdd43e0b346fed745296d
bangsep <khanhbang_ptit@yahoo.com>
2013-06-15 11:36:27 +0700

    edit short_name() function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def short_name name
+  	if name.nil?
+  		name = ""
+  	end
+
     if name.length > Settings.MAX_LENGTH_NAME
       name = name.first(Settings.MAX_LENGTH_NAME) + "..."
     end
