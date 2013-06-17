a826f6cfd3c12be41c520f757590835fc8e74641
quyetdd <quyetdd@aveglobal.net>
2013-06-17 15:10:55 +0700

    backgrou

--- db/migrate/20130516031747_create_block_login_users.rb
+++ db/migrate/20130516031747_create_block_login_users.rb
@@ -3,7 +3,7 @@ class CreateBlockLoginUsers < ActiveRecord::Migration
     create_table :block_login_users do |t|
       t.integer :user_id
       t.datetime :block_at_time
-      t.integer :login_fail_num, limit: 1
+      t.integer :login_fail_num
     end
   end
 end
