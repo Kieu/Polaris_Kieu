bb7dbb936299ffbaa443cafc0e98c9ccafe81a04
LinhNT <linhnt@linhnt-pc.(none)>
2013-06-18 15:27:24 +0700

    fix bugs #2988 2986

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   def short_name name
-  	if name.nil?
-  		name = ""
-  	end
-
     if name.length > Settings.MAX_LENGTH_NAME
       name = name.first(Settings.MAX_LENGTH_NAME) + "..."
+    end
+    name
+  end
+  
+  def short_conversion name
+    if name.length > Settings.MAX_CONVERSION_NAME
+      name = name.first(Settings.MAX_CONVERSION_NAME) + "..."
     end
     name
   end
