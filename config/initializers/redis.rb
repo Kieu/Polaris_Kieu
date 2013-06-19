91710afdf5a78641bb36d7e7f84409020030108f
quyetdd <quyetdd@aveglobal.net>
2013-06-19 15:56:32 +0700

    backgrou

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,7 @@ require "redis"
 redis_conf_path = Rails.root.join("config/redis", "#{Rails.env}.conf")
 redis_conf = File.read(redis_conf_path)
 port = /port.(\d+)/.match(redis_conf)[1]
-host_redis = /host_redis.(\w+)/.match(redis_conf)[1]
+host_redis = /host_redis.(\S+)/.match(redis_conf)[1]
 #res = `ps aux | grep redis-server`
 #unless res.include?("redis-server") && res.include?(redis_conf_path.to_s)
 #  raise "Couldn't start redis"
