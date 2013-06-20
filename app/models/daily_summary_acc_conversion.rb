7ea39341768d0c93b1cd679928287a101a33fc44
LinhNT <linhnt@linhnt-pc.(none)>
2013-06-20 17:54:14 +0700

    update model

--- app/models/daily_summary_acc_conversion.rb
+++ app/models/daily_summary_acc_conversion.rb
@@ -1,5 +1,5 @@
 class DailySummaryAccConversion < ActiveRecord::Base
-  establish_connection :"log_db_#{Rails.env}"
+  establish_connection "log_db_#{Rails.env}"
   attr_accessible :report_ymd, :account_id, :assist_count, :click_per_action,
     :conversion_id, :conversion_rate, :create_time, :first_cv_count, :id,
     :profit, :promotion_id, :repeat_cv_count, :roas, :roi, :sales,
