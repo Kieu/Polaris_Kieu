d5c639dfe9200e37e5f58da25ffe4db6eea7458e
quyetdd <quyetdd@aveglobal.net>
2013-06-24 15:29:16 +0700

    backgrou

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ $(function(){
 
     $('#promotions_list').niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px"});
     $('#conversions_list').niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px"});
-    $("#promotion_tables").niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px"});
+   // $("#promotion_tables").niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px"});
     $("#conversion_scroll").niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px"});
     $("#promotions_scroll").niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px",horizrailenabled:"false"});
 });
