c999fd0dea1b83219f972de564d59726b0ffee81
quyetdd <quyetdd@aveglobal.net>
2013-06-25 23:29:34 +0700

    backgrou

--- app/workers/export_promotions_data.rb
+++ app/workers/export_promotions_data.rb
@@ -67,7 +67,7 @@ class ExportPromotionsData
       end
 
       cnt = cnt - 1
-      CSV.open(path_file, "wb:bom|utf-8") do |csv|
+      CSV.open(path_file, "wb") do |csv|
        # io = File.open("#{Rails.root}/doc/bom_template.csv")
       #  bom = io.read(8)
         # make header for CSV file
