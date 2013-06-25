8036933018c718fd2bafc1292492972a5ccdb85d
LinhNT <linhnt@linhnt-pc.(none)>
2013-06-25 17:09:16 +0700

    update

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,15 +2,7 @@
 //検索窓内の文字
 jQuery(function($){
 	var textBox1 = $('#keywords');
-	textBox1.focus(function(){
-		if(textBox1.val()=='検索'){
-			textBox1.val('');
-		}
-	}).blur(function(){
-		if(textBox1.val()=='') {
-			textBox1.val('検索');
-		}
-	});
+	
 });
 //アカウント登録ページの
 //テキストボックス内の文字
