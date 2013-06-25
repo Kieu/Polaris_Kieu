444ea66aadaabfb26cd92a8032f6947b16ec65c2
quyetdd <quyetdd@aveglobal.net>
2013-06-26 02:49:07 +0700

    backgrou

--- app/workers/export_promotions_data.rb
+++ app/workers/export_promotions_data.rb
@@ -68,7 +68,7 @@ class ExportPromotionsData
 
       cnt = cnt - 1
       File.open(path_file, 'w') do |bom|
-        buffer = ['EF','BB','BF'].pack("H*H*H*")
+        buffer = [15711167].pack("l")
         bom.write(buffer)
       end
       return
