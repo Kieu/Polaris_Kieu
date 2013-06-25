d5f0bd7771d8f503941f3508581ac6fb12312e55
quyetdd <quyetdd@aveglobal.net>
2013-06-25 15:06:53 +0700

    backgrou

--- spec/factories/client_user.rb
+++ spec/factories/client_user.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :client_user, class: ClientsUser do
+  factory :client_user, class: ClientUser do
     client_id 1
     user_id 1
     del_flg Settings.client_user.active
