920a37ae388d5318a6b0c82b9ef8ad63c1cc9012
quyetdd <quyetdd@aveglobal.net>
2013-06-25 09:50:10 +0700

    backgrou

--- app/models/client_user.rb
+++ app/models/client_user.rb
@@ -1,5 +1,5 @@
 class ClientUser < ActiveRecord::Base
-  self.table_name "clients_users"
+  set_table_name "clients_users"
   attr_accessible :client_id, :user_id, :del_flg
   
   validates :client_id, presence: true
--- app/models/redirect_infomation.rb
+++ app/models/redirect_infomation.rb
@@ -1,5 +1,5 @@
 class RedirectInfomation < ActiveRecord::Base
-  self.table_name = "redirect_informations"
+  set_table_name "redirect_informations"
   attr_accessible :mpv, :client_id, :promotion_id, :media_category_id, :media_id, :account_id, :campaign_id, :group_id,
                                  :unit_id, :creative_id, :click_unit, :del_flg
 end
