8c1241760646c52faf1420616de736ebab02607d
quyetdd <quyetdd@aveglobal.net>
2013-06-25 10:09:06 +0700

    backgrou

--- app/controllers/background_jobs_controller.rb
+++ app/controllers/background_jobs_controller.rb
@@ -7,7 +7,7 @@ class BackgroundJobsController < ApplicationController
     job = BackgroundJob.find(params[:id])
     if current_user.id == job.user_id
       path = "#{Rails.root}/#{job.filepath}"
-      send_file(path, filename: job.filename, :type => "text/csv; charset=utf-16le", :disposition => "attachment")
+      send_file(path, filename: job.filename, :type => "text/csv; charset=utf-8")
     end
   end
   def download
