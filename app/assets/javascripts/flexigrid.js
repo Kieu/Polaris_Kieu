a924b34eabc2e3c0d6857d4e4393dbac0e266e86
quyetdd <quyetdd@aveglobal.net>
2013-06-26 17:25:10 +0700

    backgrou

--- app/assets/javascripts/flexigrid.js
+++ app/assets/javascripts/flexigrid.js
@@ -191,7 +191,7 @@
                 });
                 $(g.bDiv).css('min-height','350px');
                 $(g.bDiv).css('max-height','351px');
-                $(g.bDiv).css('width',width_body);
+                $(g.bDiv).css('width', p.width_body);
                 $(".bDiv").niceScroll({cursorcolor:"#0087A9",cursorwidth:"8px"});
             },
             dragStart: function (dragtype, e, obj) { //default drag function start
