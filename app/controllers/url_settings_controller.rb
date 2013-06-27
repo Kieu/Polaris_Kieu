6d89e64f2f1ce44aca6e24dc81cb856beabddce9
LinhNT <linhnt@linhnt-pc.(none)>
2013-06-27 17:27:02 +0700

    update

--- app/controllers/url_settings_controller.rb
+++ app/controllers/url_settings_controller.rb
@@ -89,7 +89,7 @@ class UrlSettingsController < ApplicationController
       image = url['creative']
       comment = url['comment']
       click_price = url['click_unit']
-      submit_url = "<div align='left' id='url_#{url['redirect_url_id']}'>" + Settings.DOMAIN_SUBMIT_URL + "mpv=#{url['mpv']}" + "&cid=#{client_id}&pid=#{promotion_id} </div>"
+      submit_url = "<div align='left' title='#{Settings.DOMAIN_SUBMIT_URL}" + "mpv=#{url['mpv']}" + "&cid=#{client_id}&pid=#{promotion_id}" + "'id='url_#{url['redirect_url_id']}'>" + Settings.DOMAIN_SUBMIT_URL + "mpv=#{url['mpv']}" + "&cid=#{client_id}&pid=#{promotion_id} </div>"
       submit_url += "<div align='right'><a href='javascript:void(0)'><img src='/assets/btn_copy2.gif' data-clipboard-target='url_#{url['redirect_url_id']}' class='copy_button' /></a></div>"
       if url['creative_type'] == '1'
         creative = '<img src=' + "/assets/creative/#{image}" + ' width="150" height="40" />'
